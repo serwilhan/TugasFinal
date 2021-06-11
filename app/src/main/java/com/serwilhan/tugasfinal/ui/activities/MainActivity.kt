81075def2fa3d3a1e2205854495c93a392b3aaba
“serwilhan” <serwilhan@gmail.com>
2021-06-11 19:34:56 +0800

    Edit MainActivity.kt

---
+++ app/src/main/java/com/serwilhan/tugasfinal/ui/activities/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.serwilhan.tugasfinal.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.serwilhan.tugasfinal.R
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+
+
+private const val TAG = "mainActivity"
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setupActionBarWithNavController(findNavController(R.id.nav_host_fragment))
+    }
+    
+    override fun onSupportNavigateUp() =
+        findNavController(R.id.nav_host_fragment).navigateUp() || super.onSupportNavigateUp()
+}
