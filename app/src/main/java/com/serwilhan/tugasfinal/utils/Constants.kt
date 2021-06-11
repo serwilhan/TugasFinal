a1ca09c71bb908feb3961d1dbf8be44058534e2c
“serwilhan” <serwilhan@gmail.com>
2021-06-11 20:33:23 +0800

    Add Constants.kt in utils package

---
+++ app/src/main/java/com/serwilhan/tugasfinal/utils/Constants.kt
@@ -0,0 +1,26 @@
+package com.serwilhan.tugasfinal.utils
+
+class Constants {
+
+    companion object {
+        const val CHANNEL_ID = "alarm_channel"
+        const val MAIN_NOTIFICATION_ID = 1
+
+        const val ALARM_LABEL = "alarm_name"
+        const val ALARM_TIME = "alarm_time"
+
+        const val REQUEST_CODE = "request_code"
+
+        const val TODAY = "Today"
+        const val TOMORROW = "Tomorrow"
+
+        const val CURRENT_THREAD_ID = "current_thread_id"
+
+        const val ACTION_STOP_SERVICE = "com.example.alarmapp.ACTION_STOP_SERVICE"
+        const val ACTION_START_SERVICE = "com.example.alarmapp.ACTION_START_SERVICE"
+        const val ACTION_ALARM_RECEIVER = "com.example.alarmapp.ACTION_ALARM_RECEIVER"
+
+        const val ALARM_ID = "alarm_id"
+
+    }
+}
