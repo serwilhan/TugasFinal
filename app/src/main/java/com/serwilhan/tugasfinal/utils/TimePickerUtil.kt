671a8666a8fe3bf270946aad6cb9d8dc20df63ed
“serwilhan” <serwilhan@gmail.com>
2021-06-11 20:39:31 +0800

    Add TimePickerutil.kt in utils package

---
+++ app/src/main/java/com/serwilhan/tugasfinal/utils/TimePickerUtil.kt
@@ -0,0 +1,24 @@
+package com.serwilhan.tugasfinal.utils
+
+import android.os.Build
+import android.widget.TimePicker
+
+object TimePickerUtil {
+
+    fun getTime(timePicker: TimePicker): Pair<Int, Int> {
+
+        val hour: Int
+        val minute: Int
+
+        if (Build.VERSION.SDK_INT >= 23) {
+            hour = timePicker.hour
+            minute = timePicker.minute
+        }
+        else {
+            hour = timePicker.currentHour
+            minute = timePicker.currentMinute
+        }
+        
+        return Pair(hour, minute)
+    }
+}
